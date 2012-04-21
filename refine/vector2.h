9adfe733d3c1546828921817308b0790284ee756
Barak Itkin <lightningismyname@gmail.com>
2012-04-21 12:09:04 +0300

    Add Doxygen documentation support

--- refine/vector2.h
+++ refine/vector2.h
@@ -3,15 +3,44 @@
 
 #include <glib.h>
 
+/**
+ * \struct P2trVector2
+ * A struct for representing a vector with 2 coordinates (a point in 2D)
+ */
 typedef struct {
+  /** The first coordinate of the vector */
   gdouble x;
+  /** The second coordinate of the vector */
   gdouble y;
 } P2trVector2;
 
+/** Compute the dot product of two vectors */
 gdouble       p2tr_vector2_dot       (const P2trVector2 *a, const P2trVector2 *b);
+
+/** Check if all the coordinates of the two vectors are the same */
 gboolean      p2tr_vector2_is_same   (const P2trVector2 *a, const P2trVector2 *b);
+
+/**
+ * Compute the difference of two vectors
+ * @param[in] a The vector to subtract from
+ * @param[in] b The vector to be subtracted
+ * @param[out] dest The vector in which the result should be stored
+ */
 void          p2tr_vector2_sub       (const P2trVector2 *a, const P2trVector2 *b, P2trVector2 *dest);
+
+/**
+ * Compute the norm of a vector (the length of the line from the origin
+ * to the 2D point it represents)
+ * @param[in] v The vector whose norm should be computed
+ * @return The norm of the vector
+ */
 gdouble       p2tr_vector2_norm      (const P2trVector2 *v);
+
+/**
+ * Copy a vector
+ * @param[out] dest The destination of the copy operation
+ * @param[in] src The vector to copy
+ */
 void          p2tr_vector2_copy      (P2trVector2 *dest, const P2trVector2 *src);
 
 #endif
